b89d8c6c766450c53fcb58540ba052ae008a8cb1
basakest <ab1652759879@gmail.com>
2024-09-02 22:22:17 +0800

    model factory(use RefreshDatabase trait)

--- modules/Product/Tests/ProductTest.php
+++ modules/Product/Tests/ProductTest.php
@@ -4,6 +4,5 @@ use Modules\Product\Product;
 
 it('can create a product', function () {
     $order = Product::factory()->create();
-    dd($order);
     expect($order)->toBeInstanceOf(Product::class);
 });
